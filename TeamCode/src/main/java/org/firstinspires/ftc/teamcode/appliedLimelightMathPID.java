d8d61053fb9c867c81927863a1109210fcb56a24
spicckles <coffeetree74@gmail.com>
2025-10-22 18:36:09 -0700

    organization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/appliedLimelightMathPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/appliedLimelightMathPID.java
@@ -2,4 +2,6 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.Pose2d;
 public class appliedLimelightMathPID {
     // will finish this once crosby finishing making PID
+    // now that the pid is made this is what I will be doing for the next few days
+
 }
