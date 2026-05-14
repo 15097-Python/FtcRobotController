cc00fdaa392a0fb0a4e4d11a580aee13f9dd8160
potatoduate <112445682+potatoduate@users.noreply.github.com>
2026-05-13 19:25:10 -0700

    added dual servo support
    also fixxed error with gradle

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.teamcode.Util.Servo.setDualServoPower;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+
+@TeleOp(name="servoTest")
+public class ServoTest extends LinearOpMode {
+
+    CRServo drumServo1 = hardwareMap.get(CRServo.class, "DrumServo1");
+    CRServo drumServo2 = hardwareMap.get(CRServo.class, "DrumServo2");
+    AnalogInput servoValue = hardwareMap.get(AnalogInput.class, "ServoValue");
+
+
+    @Override
+    public void runOpMode() {
+
+
+        double targetAngle = 0;
+        while (opModeIsActive()) {
+            if(gamepad1.dpad_up)targetAngle+=5;
+            if(gamepad1.dpad_down)targetAngle-=5;
+            if(gamepad1.a)targetAngle = 0;
+            setDualServoPower(targetAngle,servoValue,drumServo1,drumServo2);
+            telemetry.addData("targetAngle",targetAngle);
+            telemetry.update();
+        }
+    }
+}
+
